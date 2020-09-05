85e01caf6197c21361fb514504787a56c70c249c
JamieYang <jm.y.null@gmail.com>
2020-09-05 10:44:52 +0800

    chore: simplify template

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,12 +4,12 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
-    component: () => import(/* webpackChunkName: "home" */ '../views/module/home/index.vue'),
+    component: () => import(/* webpackChunkName: "home" */ '../views/home/index.vue'),
   },
   {
     path: '/about',
     name: 'About',
-    component: () => import(/* webpackChunkName: "about" */ '../views/module/about/index.vue'),
+    component: () => import(/* webpackChunkName: "about" */ '../views/about/index.vue'),
   },
 ]
 
