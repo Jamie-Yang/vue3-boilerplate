ba9849193d1526ffc50f2857075e123399fdfc8c
JamieYang <jm.y.null@gmail.com>
2021-01-08 00:13:51 +0800

    feat:  proposal supported

--- babel.config.js
+++ babel.config.js
@@ -3,12 +3,14 @@ module.exports = {
     [
       '@babel/preset-env',
       {
-        useBuiltIns: 'usage',
-        modules: false,
-        corejs: 3,
+        useBuiltIns: 'usage', // adds specific imports for polyfills when they are used in each file.
+        modules: false, // preserve ES modules.
+        corejs: { version: 3, proposals: true }, // enable polyfilling of every proposal supported by core-js.
       },
     ],
   ],
-  plugins: ['@babel/plugin-transform-runtime'],
+  plugins: [
+    '@babel/plugin-transform-runtime', // enables the re-use of Babel's injected helper code to save on codesize.
+  ],
   exclude: [/core-js/],
 }
